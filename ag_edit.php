8484d1d3d91fc11e7d42a7e0e61955818194d2f1
Dimas Satria <dimassatriia@gmail.com>
2020-01-13 13:46:07 +0700

    edit fitur ag fix

--- ag_edit.php
+++ ag_edit.php
@@ -1,8 +1,36 @@
-<?php include("header.php");
-require("koneksi.php"); ?>
+<?php 
+include("guard/guard_1.php");
+include("header.php");
+require("koneksi.php");
+
+$id = $_SESSION['id'];
+
+
+//option untuk menampilkan spv yang dibawahinya
+$querySpv = "SELECT * FROM `supervisor` WHERE `id_agency` = $id";
+$runQuerySpv = mysqli_query($con,$querySpv);
+
+while ($dataSpv = mysqli_fetch_assoc($runQuerySpv)) {
+    $kumpulanDataSpv[] = $dataSpv;
+}
+// end
+
+//option untuk menampilkan partner yang dibawahinya
+$queryPartner = "SELECT salesforce.id_salesforce as id_salesforce, salesforce.nama as nama FROM `salesforce` INNER JOIN `supervisor` ON salesforce.id_supervisor = supervisor.id_supervisor WHERE supervisor.id_agency = $id";
+$runQueryPartner = mysqli_query($con,$queryPartner);
+
+while ($dataPartner = mysqli_fetch_assoc($runQueryPartner)) {
+    $kumpulanDataPartner[] = $dataPartner;
+}
+// end
+
+
+?>
+
+
 <body>
 
-<?php
+    <?php
 include("sidebar/sidebar_dataplg_ag.php");
 if(isset($_GET['id'])){
 ?>
@@ -14,7 +42,7 @@ if(isset($_GET['id'])){
                 </div>
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
-                            </a>
+                        </a>
                         </li>
                     </ul>
                 </div>
@@ -32,16 +60,18 @@ if(isset($_GET['id'])){
                             <div class="content table-responsive table-full-width">
                                 <table class="table table-striped">
 
-                    <div class="col-md-12">
-                        <div class="card card-plain">
-                            <div class="header">
-                                <h2 class="title"><center><b>Edit Data</b></center></h2>
-                            </div>
-                            <div class="content table-responsive table-full-width">
-                                <table class="table table-hover">
-                                    <tbody>
+                                    <div class="col-md-12">
+                                        <div class="card card-plain">
+                                            <div class="header">
+                                                <h2 class="title">
+                                                    <center><b>Edit Data</b></center>
+                                                </h2>
+                                            </div>
+                                            <div class="content table-responsive table-full-width">
+                                                <table class="table table-hover">
+                                                    <tbody>
 
-<?php
+                                                        <?php
 require("koneksi.php");
 $id = $_GET['id'];
 $query = "SELECT * FROM data_pelanggan WHERE id = '$id'";
@@ -64,86 +94,124 @@ $id_partner = $data['id_partner'];
 $id_spv = $data['id_spv'];
 
 ?>
-		<form method='post' action='ag_update.php' onsubmit='return confirm("Apakah data sudah benar?");'>
-                           <div class='col-md-4'>
-                            <div class='form-group'>
-                                <label>Track Id</label>
-                                <input type='text' class='form-control border-input' value='<?php echo $track_id ?>' disabled>
-                                <input type='text' class='form-control border-input' name='track_id' value='<?php echo $track_id ?>' style="display:none">
-                            </div>
-		              	        <div class='form-group'>
-                                <label>Nama Pelanggan</label>
-                                <input type='text' class='form-control border-input' name='nama_pelanggan' value='<?php echo $nama_pelanggan ?>'  autocomplete="off" required>
-                            </div>
-                            <div class='form-group'>
-                                <label>Alamat</label>
-                                <input type='text' class='form-control border-input' name='alamat' value='<?php echo $alamat ?>'  autocomplete="off" required>
-                            </div>
-                            <div class='form-group'>
-                                <label>No KTP</label>
-                                <input type='text' class='form-control border-input' name='ktp' value='<?php echo $ktp ?>'  autocomplete="off" required>
-                            </div>
-                            <div class='form-group'>
-                                <label>STO</label>
-                                <input type='text' class='form-control border-input' name='sto' value='<?php echo $sto ?>'  autocomplete="off" required>
-                            </div>
-                            <div class='form-group'>
-                                <label>Second CP</label>
-                                <input type='text' class='form-control border-input' name='second_cp' value='<?php echo $second_cp ?>'  autocomplete="off" required>
-                            </div>
-                            <div class='form-group'>
-                                <label>Paket</label>
-                                <input type='text' class='form-control border-input' name='paket' value='<?php echo $paket ?>'  autocomplete="off" required>
-                            </div>
-                            <div class='form-group'>
-                                <label>Tagging Rill</label>
-                                <input type='text' class='form-control border-input' name='tagging_rill' value='<?php echo $tagging_rill ?>'  autocomplete="off" required>
-                            </div>
-                            <div class='form-group'>
-                                <label>ODP</label>
-                                <input type='text' class='form-control border-input' name='odp' value='<?php echo $odp ?>'  autocomplete="off" required>
-                            </div>
-                            <div class='form-group'>
-                                <label>ODP ke Pelanggan</label>
-                                <input type='text' class='form-control border-input' name='odp_ke_pelanggan' value='<?php echo $odp_ke_pelanggan ?>'  autocomplete="off" required>
-                            </div>
-                            <div class='form-group'>
-                                <label>Agency</label>
-                                <input type='hidden' class='form-control border-input' name='id_agency' value='<?php echo $id_agency ?>'  autocomplete="off" required>
-                            </div>
-                            <div class='form-group'>
-                                <label>Partner</label>
-                                <input type='text' class='form-control border-input' name='id_partner' value='<?php echo $id_partner ?>'  autocomplete="off" required>
-                            </div>
-                            <div class='form-group'>
-                                <label>SPV</label>
-                                <input type='text' class='form-control border-input' name='id_spv' value='<?php echo $id_spv ?>'  autocomplete="off" required>
-                            </div>
+                                                        <form method='post' action='ag_update.php'
+                                                            onsubmit='return confirm("Apakah data sudah benar?");'>
+                                                            <div class='col-md-4'>
+                                                                <div class='form-group'>
+                                                                    <label>Track Id</label>
+                                                                    <input type='text' class='form-control border-input'
+                                                                        value='<?php echo $track_id ?>' disabled>
+                                                                    <input type='text' class='form-control border-input'
+                                                                        name='track_id' value='<?php echo $track_id ?>'
+                                                                        style="display:none">
+                                                                </div>
+                                                                <div class='form-group'>
+                                                                    <label>Nama Pelanggan</label>
+                                                                    <input type='text' class='form-control border-input'
+                                                                        name='nama_pelanggan'
+                                                                        value='<?php echo $nama_pelanggan ?>'
+                                                                        autocomplete="off" required>
+                                                                </div>
+                                                                <div class='form-group'>
+                                                                    <label>Alamat</label>
+                                                                    <input type='text' class='form-control border-input'
+                                                                        name='alamat' value='<?php echo $alamat ?>'
+                                                                        autocomplete="off" required>
+                                                                </div>
+                                                                <div class='form-group'>
+                                                                    <label>No KTP</label>
+                                                                    <input type='text' class='form-control border-input'
+                                                                        name='ktp' value='<?php echo $ktp ?>'
+                                                                        autocomplete="off" required>
+                                                                </div>
+                                                                <div class='form-group'>
+                                                                    <label>STO</label>
+                                                                    <input type='text' class='form-control border-input'
+                                                                        name='sto' value='<?php echo $sto ?>'
+                                                                        autocomplete="off" required>
+                                                                </div>
+                                                                <div class='form-group'>
+                                                                    <label>Second CP</label>
+                                                                    <input type='text' class='form-control border-input'
+                                                                        name='second_cp'
+                                                                        value='<?php echo $second_cp ?>'
+                                                                        autocomplete="off" required>
+                                                                </div>
+                                                                <div class='form-group'>
+                                                                    <label>Paket</label>
+                                                                    <input type='text' class='form-control border-input'
+                                                                        name='paket' value='<?php echo $paket ?>'
+                                                                        autocomplete="off" required>
+                                                                </div>
+                                                                <div class='form-group'>
+                                                                    <label>Tagging Rill</label>
+                                                                    <input type='text' class='form-control border-input'
+                                                                        name='tagging_rill'
+                                                                        value='<?php echo $tagging_rill ?>'
+                                                                        autocomplete="off" required>
+                                                                </div>
+                                                                <div class='form-group'>
+                                                                    <label>ODP</label>
+                                                                    <input type='text' class='form-control border-input'
+                                                                        name='odp' value='<?php echo $odp ?>'
+                                                                        autocomplete="off" required>
+                                                                </div>
+                                                                <div class='form-group'>
+                                                                    <label>ODP ke Pelanggan</label>
+                                                                    <input type='text' class='form-control border-input'
+                                                                        name='odp_ke_pelanggan'
+                                                                        value='<?php echo $odp_ke_pelanggan ?>'
+                                                                        autocomplete="off" required>
+                                                                </div>
+                                                                <div class='form-group'>
+                                                                    <input type='hidden'
+                                                                        class='form-control border-input'
+                                                                        name='id_agency'
+                                                                        value='<?php echo $id_agency ?>'
+                                                                        autocomplete="off" required>
+                                                                </div>
+                                                                <div class='form-group'>
+                                                                    <label>Partner</label>                                                                   
+                                                                        <select class="form-control border-input" name="id_partner" autocomplete="off" required>
+                                                                        <?php foreach($kumpulanDataPartner as $partner) : ?>
+                                                                            <option value="<?=$partner['id_salesforce'] ?> " <?php if($id_partner == $partner['id_salesforce']): echo 'selected'; endif;?>><?= $partner['nama'] ?></option>
+                                                                        <?php endforeach ?>                                
+                                                                        </select>
+                                                                </div>
+                                                                <div class='form-group'>
+                                                                    <label>SPV</label>
+                                                                        <select class="form-control border-input" name="id_spv" autocomplete="off" required>
+                                                                        <?php foreach($kumpulanDataSpv as $spv) : ?>
+                                                                            <option value="<?=$spv['id_supervisor'] ?> " <?php if($id_spv == $spv['id_supervisor']): echo 'selected'; endif;?>><?= $spv['nama'] ?></option>
+                                                                        <?php endforeach ?>                                
+                                                                        </select>
+                                                                </div>
 
-				<br>
-                            <button type="submit" class="btn btn-success" name='btn-update'>Simpan</button>
+                                                                <br>
+                                                                <button type="submit" class="btn btn-success"
+                                                                    name='btn-update'>Simpan</button>
+                                                            </div>
+                                                        </form>
+
+                                                    </tbody>
+                                                </table>
+
+                                            </div>
+                                        </div>
                                     </div>
-                                </form>
-
-                                    </tbody>
-                                </table>
 
                             </div>
                         </div>
                     </div>
 
-                </div>
-            </div>
-        </div>
-
-<?php
+                    <?php
 include("footer.php");
 }
 else{ ?>
- <script language="JavaScript">
- alert("Pilih item terlebih dahulu");
- </script>
-<?php
+                    <script language="JavaScript">
+                    alert("Pilih item terlebih dahulu");
+                    </script>
+                    <?php
 include("ag_tampil.php");
 }
 ?>
